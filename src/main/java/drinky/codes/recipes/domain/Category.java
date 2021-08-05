706125bec255e942c929ea2a71d2d6aada991dd0
Uptaker <markus.tammeoja@gmail.com>
2021-08-05 22:15:36 +0300

    Add full lombok refactor. Fixed with @EqualsAndHashCode in entities and @Transactional in bootstrap

--- src/main/java/drinky/codes/recipes/domain/Category.java
+++ src/main/java/drinky/codes/recipes/domain/Category.java
@@ -5,6 +5,8 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.Set;
 
+@Data
+@EqualsAndHashCode(exclude = {"recpies"})
 @Entity
 public class Category {
     @Id
@@ -15,28 +17,4 @@ public class Category {
 
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
