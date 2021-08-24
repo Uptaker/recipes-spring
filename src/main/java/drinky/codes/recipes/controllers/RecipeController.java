af139038ddf187b5a80c6f4368985e9edd8555b8
Uptaker <markus.tammeoja@gmail.com>
2021-08-25 01:06:27 +0300

    Add display recipe by ID

---
+++ src/main/java/drinky/codes/recipes/controllers/RecipeController.java
@@ -0,0 +1,28 @@
+package drinky.codes.recipes.controllers;
+
+import drinky.codes.recipes.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showRecipeById(@PathVariable Long id, Model model) {
+        log.debug("Getting recipe page");
+        model.addAttribute("recipe", recipeService.findById(id));
+
+        return "recipe/show";
+    }
+}
